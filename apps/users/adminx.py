a66a5f2f22b347873cb8e2f6ed67ce5cdead57b4
linchong <383105690@qq.com>
2017-11-08 16:53:08 +0800

    motify xadmin globalsetting

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,6 +1,18 @@
 import xadmin
+from xadmin import views
 
 from .models import EmailVerificationCode, Banner
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = '慕学网后台管理系统'
+    site_footer = '慕学网'
+    menu_style = 'accordion'
 
 
 class EmailVerificationCodeAdmin(object):
@@ -17,3 +29,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerificationCode, EmailVerificationCodeAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
