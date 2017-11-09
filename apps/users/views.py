dfbffc9155ea9616b58447ce508c6d75df777d39
linchong <383105690@qq.com>
2017-11-09 18:01:38 +0800

    add login function

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic.base import View
+
+from .forms import LoginForm
+from .models import UserInfo
+
 
 # Create your views here.
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        user = UserInfo.objects.get(Q(username=username)|Q(email=username)|Q(mobile=username))
+        if user.check_password(password):
+            return user
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'login.html', {})
+
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return render(request, 'index.html', {})
+            else:
+                return render(request, 'login.html', {'msg': '用户名或密码错误！'})
+        else:
+            return render(request, 'login.html', {'login_form': login_form})
--- muxuewang/urls.py
+++ muxuewang/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 import xadmin
+
+from users.views import Login
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^login/$', Login.as_view(), name='login')
 ]
